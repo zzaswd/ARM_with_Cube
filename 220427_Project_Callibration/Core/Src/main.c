186bacb81c9bc511ec4cd3243fb4351ed3aaca00
zzaswd <gkffhdnd123@gmail.com>
2022-04-27 12:49:32 +0900

    나머지

--- 220427_Project_Callibration/Core/Src/main.c
+++ 220427_Project_Callibration/Core/Src/main.c
@@ -353,7 +353,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 64;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 20000-1;
+  htim3.Init.Period = 10000-1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
